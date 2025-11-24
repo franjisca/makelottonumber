ce1aa72a6efa1551a55639299172e34932687a01
JIYEON <publicjy324@gmail.com>
2025-11-24 22:49:45 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // GitHub Pages를 위한 상대 경로 설정
+  base: '/makelottonumber/', // GitHub Pages를 위한 상대 경로 설정
   build: {
     outDir: 'dist'
   }
